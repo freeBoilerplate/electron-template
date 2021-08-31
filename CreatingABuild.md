7f8743bafabd737e3f84064cfd177bb6af2ca510
brandonpessman <pessman.brandon@gmail.com>
2021-08-31 18:18:03 -0500

    Init project

---
+++ CreatingABuild.md
@@ -0,0 +1,2 @@
+// Windows 64-Bit Build
+npx electron-packager ./game GameDemo --platform=win32 --arch=x64
---
+++ src/App.js
@@ -0,0 +1,11 @@
+import './App.css';
+
+function App() {
+  return (
+    <div>
+      <h1>This is a game.</h1>
+    </div>
+  );
+}
+
+export default App;
