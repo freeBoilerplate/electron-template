dd9733e872e77658d3c45352a16a9ffa3c31b574
BrandonPessman <pessman.brandon@gmail.com>
2022-05-03 19:29:34 -0500

    chore: upgrading dependencies

--- src/App.js
+++ src/App.js
@@ -1,13 +1,6 @@
 import React, { useEffect } from 'react'
 import './App.css';
 
-// Libary Imports
-import { HashRouter, Link, Route, Switch } from "react-router-dom";
-
-// Game Pages
-import Home from './components/Home'
-import Debug from './components/Debug'
-
 function App() {
   
   useEffect(() => {
@@ -15,16 +8,9 @@ function App() {
   }, [])
   
   return (
-    <HashRouter>
-      <div className="menu">
-        <Link to="/"><h2>Home</h2></Link>
-        <Link to="/debug"><h2>Debug</h2></Link>
-      </div>
-      <Switch>
-        <Route exact path="/" component={Home}/>
-        <Route exact path="/debug" component={Debug}/>
-      </Switch> 
-    </HashRouter>
+    <div>
+      <h1>Testing</h1>
+    </div>
   );
 }
 
